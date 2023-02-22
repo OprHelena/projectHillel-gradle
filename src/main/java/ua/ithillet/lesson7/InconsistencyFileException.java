f73cf82175a8d57f0dd322ff1c8f334d871738cc
OprHelena <opr.helena@gmail.com>
2023-02-22 17:06:58 +0100

    HW_7

---
+++ src/main/java/ua/ithillet/lesson7/InconsistencyFileException.java
@@ -0,0 +1,9 @@
+package ua.ithillet.lesson7;
+
+public class InconsistencyFileException extends RuntimeException {
+
+    public InconsistencyFileException(String message) {
+        super(message);
+    }
+
+}
