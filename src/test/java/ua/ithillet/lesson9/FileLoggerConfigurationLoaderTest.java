d22f2e7309a0d443dd6c6b989bc7b12b0bcf5e8a
OprHelena <opr.helena@gmail.com>
2023-03-14 16:17:50 +0100

    HW9 log file

---
+++ src/test/java/ua/ithillet/lesson9/FileLoggerConfigurationLoaderTest.java
@@ -0,0 +1,17 @@
+package ua.ithillet.lesson9;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FileLoggerConfigurationLoaderTest {
+
+    @Test
+    public void checkLoadLogs() {
+        FileLoggerConfigurationLoader loader = new FileLoggerConfigurationLoader();
+        FileLoggerConfiguration configFile = loader.load("src/main/resources/log_config.properties");
+
+        Assertions.assertEquals("log", configFile.getPath());
+        Assertions.assertEquals(LoggingLevel.DEBUG, configFile.getLevel());
+        Assertions.assertEquals(200000, configFile.getMaxSize());
+    }
+}
